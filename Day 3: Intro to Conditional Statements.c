594c1697ebb2730f23fbf111b31729e86286b0f5
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-16 13:48:01 +0530

    Update Day 3: Intro to Conditional Statements.c

--- Day 3: Intro to Conditional Statements.c
+++ Day 3: Intro to Conditional Statements.c
@@ -6,3 +6,21 @@
 #include <limits.h>
 #include <stdbool.h>
 
+int main() {
+    int N; 
+    scanf("%d",&N);
+
+    if(N % 2 != 0) {
+        printf("Weird\n");
+    } else {
+        if(N >= 2 && N < 5) {
+            printf("Not Weird\n");
+        } else if(N > 5 && N <= 20) {
+            printf("Weird\n");
+        } else {
+            printf("Not Weird\n");
+        }
+    }
+
+    return 0;
+}
